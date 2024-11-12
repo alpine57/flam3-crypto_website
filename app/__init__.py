b3ba6cee14084af25532625800dcf9ce3f495a56
alpine57 <kamutileeon@gmail.com>
2024-11-12 03:36:38 -0800

    secret key updated

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from .routes import bp as main_bp
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your_secret_key_here'  # Set a real secret key here
+    app.config['SECRET_KEY'] =  '9f80b09595a52aee7f1910222adc2255'  # Set a real secret key here
     app.register_blueprint(main_bp)
     return app
 
